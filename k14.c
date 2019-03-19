8b3b5103b4caf8cd89c5871b5ab6d4e616bb26d6
prasadkaruna <46813904+prasadkaruna@users.noreply.github.com>
2019-03-19 15:59:53 +0530

    Create k14.c

---
+++ k14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b,i,k;
+  scanf("%d%d",&a,&b);
+  for(i=a+1;i<=b;i++)
+  {
+    k=0;
+    if(i%2==0)
+    {
+  k=1;
+  }
+    if(k==0)
+    {
+      printf(" %d",i);
+    }
+  }
+}
